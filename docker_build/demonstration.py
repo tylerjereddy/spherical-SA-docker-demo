b5dd2ee2d1f334ba44cf4b687cadc3a238cef202
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-04-15 10:36:25 +0100

    Streamlined demonstration.py for testing purposes and fixed a few bugs.

--- docker_build/demonstration.py
+++ docker_build/demonstration.py
@@ -3,9 +3,16 @@
 Authors: Tyler Reddy and Edward Edmondson'''
 
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot
 import numpy as np
+import scipy
+import scipy.spatial
 import math
+from matplotlib import colors
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from mpl_toolkits.mplot3d import proj3d
+from mpl_toolkits.mplot3d.spherical import SphericalPolygon
 
 def convert_cartesian_array_to_spherical_array(coord_array,angle_measure='radians'):
     '''Take shape (N,3) cartesian coord_array and return an array of the same shape in spherical polar form (r, theta, phi). Based on StackOverflow response: http://stackoverflow.com/a/4116899
@@ -71,25 +78,58 @@ def calculate_surface_area_of_a_spherical_Voronoi_polygon(array_ordered_Voronoi_
         totalexcess += 4 * math.atan(math.sqrt( math.tan(0.5 * s) * math.tan(0.5 * (s-root_a_dist)) * math.tan(0.5 * (s-root_b_dist)) * math.tan(0.5 * (s-a_b_dist))))
     return totalexcess * (sphere_radius ** 2)
 
-if __name__ == '__main__':
+def plot_ref_unit_sphere(ax):
+    # plot the unit sphere for reference 
+    u = np.linspace(0, 2 * np.pi, 100)
+    v = np.linspace(0, np.pi, 100)
+    x = np.outer(np.cos(u), np.sin(v))
+    y = np.outer(np.sin(u), np.sin(v))
+    z = np.outer(np.ones_like(u), np.cos(v))
+    ax.plot_surface(x, y, z, rstride=5, cstride=5, color='y', alpha=0.1)
+    ax.azim = 90
+    ax.elev = 30
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
 
+def perform_sample_calculation_and_plot(spherical_polygon_vertices, fraction_unit_sphere_area_expected, order, output_file):
+    output_file.write('input spherical polygon vertices:' + str(spherical_polygon_vertices) + '\n')
     #the surface area of a sphere is given by 4 * math.pi * r ** 2
     unit_sphere_surface_area = 4 * math.pi
+    expected_SA = unit_sphere_surface_area * fraction_unit_sphere_area_expected
+    output_file.write('expected_SA for {fraction} fraction of unit sphere:'.format(fraction=fraction_unit_sphere_area_expected) + str(expected_SA) + '\n')
+    calculated_SA = calculate_surface_area_of_a_spherical_Voronoi_polygon(spherical_polygon_vertices,1.0)
+    output_file.write('calculated_SA for {fraction} fraction of unit sphere:'.format(fraction=fraction_unit_sphere_area_expected) + str(calculated_SA) + '\n')
+    fig = matplotlib.pyplot.figure()
+    ax = fig.add_subplot(111, projection = '3d')
+    plot_ref_unit_sphere(ax)
+    #old style matplotlib spherical polygon
+    polygon = Poly3DCollection([spherical_polygon_vertices], alpha=1.0)
+    polygon.set_color('blue')
+    ax.add_collection3d(polygon)
+    fig.savefig('{fraction}_plot_old_order_{order}.png'.format(fraction=fraction_unit_sphere_area_expected, order=order), dpi = 300)
+    #new style matplotlib spherical polygon (Nikolai's branch)
+    fig2 = matplotlib.pyplot.figure()
+    ax = fig2.add_subplot(111, projection = '3d')
+    plot_ref_unit_sphere(ax)
+    polygon = SphericalPolygon(spherical_polygon_vertices) 
+    polygon.add_to_ax(ax, alpha=1.0, color='blue')
+    fig2.savefig('{fraction}_plot_new_{order}.png'.format(fraction=fraction_unit_sphere_area_expected, order=order), dpi = 300)
 
-    #calculate the surface area of a spherical polygon covering 1/8th the surface area of the unit sphere
-    #compare with expected value
-    expected_one_eighth_SA = unit_sphere_surface_area / 8.
-    one_eighth_polygon_vertices = np.array([[1,0,0],[0,0,1],[0,-1,0]])
-    calculated_one_eighth_SA = calculate_surface_area_of_a_spherical_Voronoi_polygon(one_eighth_polygon_vertices,1.0)
-    print 'expected_one_eighth_SA:', expected_one_eighth_SA
-    print 'calculated_one_eighth_SA:', calculated_one_eighth_SA
-    
-    
-    #repeat for 3/8ths of a unit sphere
-    expected_three_eighth_SA = unit_sphere_surface_area * (3./8.)
-    three_eighth_polygon_vertices = np.array([[0,0,1],[1,0,0],[0,1,0],[-1,0,0],[0,-1,0]]) #sensitive to specified order!
-    calculated_three_eighth_SA = calculate_surface_area_of_a_spherical_Voronoi_polygon(three_eighth_polygon_vertices,1.0)
-    print 'expected_three_eighth_SA:', expected_three_eighth_SA
-    print 'calculated_three_eighth_SA:', calculated_three_eighth_SA
+if __name__ == '__main__':
 
-    #I can't get the surface area calculation to work for a hemisphere, but then how does one provide the 'ordered' vertices for a hemisphere??
+    with open('log.txt', 'w') as output_file:
+
+        one_eighth_polygon_vertices = np.array([[0, 1., 0], [0, 0, 1.], [-1., 0, 0]]) 
+        perform_sample_calculation_and_plot(spherical_polygon_vertices = one_eighth_polygon_vertices, fraction_unit_sphere_area_expected = 1./8., order=1, output_file=output_file)
+
+        one_eighth_polygon_vertices_2 = np.array([[1,0,0],[0,0,1],[0,-1,0]]) #Nikolai's plotting code doesn't plot this set of vertices for some reason
+        perform_sample_calculation_and_plot(spherical_polygon_vertices = one_eighth_polygon_vertices_2, fraction_unit_sphere_area_expected = 1./8., order=2, output_file=output_file)
+
+        three_eighths_polygon_vertices = np.array([[0,0,1],[1,0,0],[0,1,0],[-1,0,0],[0,-1,0]]) #sensitive to specified order!
+        perform_sample_calculation_and_plot(spherical_polygon_vertices = three_eighths_polygon_vertices, fraction_unit_sphere_area_expected = 3./8., order=1, output_file=output_file)
+
+        three_eighths_polygon_vertices_2 = np.array([[1,0,0],[0,1,0],[-1,0,0],[0,-1,0],[0,0,1]]) #sensitive to specified order!
+        perform_sample_calculation_and_plot(spherical_polygon_vertices = three_eighths_polygon_vertices_2, fraction_unit_sphere_area_expected = 3./8., order=2, output_file=output_file)
+
+        #I can't get the surface area calculation to work for a hemisphere, but then how does one provide the 'ordered' vertices for a hemisphere??
